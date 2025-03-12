a9f745427e5aa288ca6673ecd4aa939bf624fcb0
rudsonalves <rudsonalves67@gmail.com>
2025-03-12 15:56:56 -0300

    HotReload em ação.

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -14,13 +14,18 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Inherited Widget'),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
+          spacing: 64,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FlutterLogo(size: 100),
-            Text("Olá, Flutter!"),
+            Text(
+              "Olá, Flutter!",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
             FilledButton.icon(
               onPressed: () {},
               label: Text("Clique Aqui"),
